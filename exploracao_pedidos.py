784b92856a43b5884fdeaf545acfa2994f24daf2
murilocast1704 <murilocastilho3@gmail.com>
2026-03-16 19:46:42 +0000

    Atualizações finais

--- exploracao_pedidos.py
+++ exploracao_pedidos.py
@@ -3,29 +3,30 @@ import pyspark.sql.functions as F
 
 spark = SparkSession.builder.appName("dataeng-exemplo-dataframe").getOrCreate()
 
-# schema = """
-# ID_PEDIDO STRING,
-# PRODUTO STRING,
-# VALOR_UNITARIO Decimal(10,2), 
-# QUANTIDADE int,
-# DATA_CRIACAO timestamp,
-# UF STRING,
-# ID_CLIENTE int """
+schema = """
+ID_PEDIDO STRING,
+PRODUTO STRING,
+VALOR_UNITARIO Decimal(10,2), 
+QUANTIDADE int,
+DATA_CRIACAO timestamp,
+UF STRING,
+ID_CLIENTE int """
 
-# df = spark.read \
-#     .format("csv") \
-#     .option("sep", ";") \
-#     .option("header", True) \
-#     .schema(schema) \
-#     .load("./projeto/datasets-csv-pedidos/data/pedidos/*")
+df = spark.read \
+    .format("csv") \
+    .option("sep", ";") \
+    .option("header", True) \
+    .schema(schema) \
+    .load("./projeto/datasets-csv-pedidos/data/pedidos/*")
 
-# df.printSchema()
+df.printSchema()
 
-# df.show()
+df.show()
 
-# df.filter(df.ID_PEDIDO == "b041ce31-10ae-4af1-becc-68507500cf2a").show()
+df.filter(df.ID_PEDIDO == "b041ce31-10ae-4af1-becc-68507500cf2a").show()
 
 
+## Ler o resultado do pipeline
 df_resultado = spark.read.parquet("projeto/output/relatorio_pedidos/")
 df_resultado.printSchema()
 print("Total de linhas:", df_resultado.count())
---
+++ resultado.py
@@ -0,0 +1,6 @@
+from pyspark.sql import SparkSession
+spark = SparkSession.builder.master("local[*]").getOrCreate()
+
+df = spark.read.parquet("./pyspark-pedidos/output/relatorio_pedidos")
+df.printSchema()
+df.show()
