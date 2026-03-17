47605e7204ba48fce4d36fc074d7f81552643c9f
Murilo Castilho <167326259+murilocast1704@users.noreply.github.com>
2026-03-16 22:29:26 -0300

    Update parquet file path in resultado.py

--- resultado.py
+++ resultado.py
@@ -1,6 +1,6 @@
 from pyspark.sql import SparkSession
 spark = SparkSession.builder.master("local[*]").getOrCreate()
 
-df = spark.read.parquet("./pyspark-pedidos/output/relatorio_pedidos")
+df = spark.read.parquet("/home/ubuntu/environment/pyspark-pedidos/output/relatorio_pedidos")
 df.printSchema()
 df.show(20)
